ab102ce813a5dd27e61e773ecc96b5e945e065b4
Joe Green <joe.green@smrtr.co.uk>
2013-12-17 13:13:18 +0000

    Removed phpunit autoloader in favour of composer autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 define('TESTS_PATH', realpath(dirname(__FILE__)));
 define('ROOT_PATH', realpath(dirname(TESTS_PATH)));
-require_once ('PHPUnit/Autoload.php');
+//require_once ('PHPUnit/Autoload.php');
 require_once(ROOT_PATH.'/vendor/autoload.php');
 
 class Smrtr_DataGrid_ControllerTestCase extends PHPUnit_Framework_TestCase
